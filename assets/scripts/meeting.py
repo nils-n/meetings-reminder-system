d1e21a5b4b9bd67160c407269c2c7c40b3cf9039
Nils <52067471+nils-n@users.noreply.github.com>
2023-05-10 15:30:55 +0100

    add validation method that meeting name is a string

--- assets/scripts/meeting.py
+++ assets/scripts/meeting.py
@@ -13,3 +13,14 @@ class Meeting :
     room_confirmed: bool = False
     participants: list[Participant] = field( default_factory=list)
     meeting_notes : str = ""
+
+    def __post_init__(self):
+        """validate that the attributes have correct form"""
+        self.validate_name( self.name)
+
+    def validate_name(self, new_name):
+        """validate that the new name is a string type""" 
+        if not isinstance(new_name, str):
+            raise TypeError(f'The meeting name should be of type string \
+                             ( {new_name} is not a string)')
+        
