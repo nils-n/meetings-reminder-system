28da2ba4a3e74fb5df2496836c6560ea35ecdc6f
Nils <52067471+nils-n@users.noreply.github.com>
2023-05-10 15:26:18 +0100

    add failing test for Meeting name

--- assets/scripts/test_meeting.py
+++ assets/scripts/test_meeting.py
@@ -9,7 +9,9 @@ A ssert
 
 """
 from datetime import datetime
+from contextlib import nullcontext as does_not_raise
 from meeting import Meeting
+import pytest
 
 def test_can_create_new_meeting( create_random_participants) -> None:
     """Test if a meeting instance can be created"""
@@ -19,7 +21,27 @@ def test_can_create_new_meeting( create_random_participants) -> None:
     random_date_time = datetime.now()
     random_meeting_notes = 'This is a random note for this meeting \
         to be sent along with the reminder. '
+
     model = Meeting(random_meeting_id, random_meeting_name, random_date_time, True, True, \
                      participants, random_meeting_notes)
 
     assert isinstance( model, Meeting)
+
+@pytest.mark.parametrize (
+        "fixture_name, new_name, expectation", 
+        [
+            ("create_random_participants", 'Death Star Staff Meeting', does_not_raise()),
+            ("create_random_participants", 'Rebel Briefing', does_not_raise()),
+            ("create_random_participants", 42,  pytest.raises(TypeError))
+        ]
+)
+def test_invalid_name_raises_type_error(fixture_name, new_name, expectation, request):
+    """test various correct and incorrect meeting names"""
+    participants = request.getfixturevalue(fixture_name)
+    random_meeting_id = 42
+    random_date_time = datetime.now()
+    random_meeting_notes = 'This is a random note for this meeting.'
+
+    with expectation:
+        Meeting(random_meeting_id, new_name, random_date_time, True, True, \
+                     participants, random_meeting_notes)
