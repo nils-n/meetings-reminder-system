5511717806a95724c81a667fba228c7d09fd61c0
Nils <52067471+nils-n@users.noreply.github.com>
2023-05-23 17:25:44 +0100

    add a mock implementation to display meetings as DataTable

--- run.py
+++ run.py
@@ -14,18 +14,18 @@ from textual.widgets import DataTable
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 # this is just the test how to work with Data tables in textualize - and how they would look like on heroku
-ROWS = [
-    ("lane", "swimmer", "country", "time"),
-    (4, "Joseph Schooling", "Singapore", 50.39),
-    (2, "Michael Phelps", "United States", 51.14),
-    (5, "Chad le Clos", "South Africa", 51.14),
-    (6, "László Cseh", "Hungary", 51.14),
-    (3, "Li Zhuhao", "China", 51.26),
-    (8, "Mehdy Metella", "France", 51.58),
-    (7, "Tom Shields", "United States", 51.73),
-    (1, "Aleksandr Sadovnikov", "Russia", 51.84),
-    (10, "Darren Burns", "Scotland", 51.84),
-]
+# ROWS = [
+#     ("lane", "swimmer", "country", "time"),
+#     (4, "Joseph Schooling", "Singapore", 50.39),
+#     (2, "Michael Phelps", "United States", 51.14),
+#     (5, "Chad le Clos", "South Africa", 51.14),
+#     (6, "László Cseh", "Hungary", 51.14),
+#     (3, "Li Zhuhao", "China", 51.26),
+#     (8, "Mehdy Metella", "France", 51.58),
+#     (7, "Tom Shields", "United States", 51.73),
+#     (1, "Aleksandr Sadovnikov", "Russia", 51.84),
+#     (10, "Darren Burns", "Scotland", 51.84),
+# ]
 
 cursors = cycle(["column", "row", "cell"])
 
@@ -34,9 +34,12 @@ class MockMeeting():
     mocks a Meeting class 
     eventually this class will be imported. For now, this is just a placeholder
     """
-    def __init__(self, name, ) -> None:
+    def __init__(self, meeting_id, name, time, invited, confirmed ) -> None:
+        self.meeting_id = meeting_id
         self.name = name
-        self.num_participants = 0
+        self.time = time
+        self.num_invited = invited
+        self.num_confirmed = confirmed
         self.participants = []
 
 
@@ -50,7 +53,9 @@ class MockSchedule():
         self.name = name
         self.num_meetings = 0
         self.meetings = []
+        self.table_rows = []
         self.load_meetings()
+        self.convert_meetings_to_table()
 
     def change_name(self, new_name):
         """ just to test if this method can be called from the main App """
@@ -58,37 +63,32 @@ class MockSchedule():
 
     def load_meetings(self):
         """ create mock list of meetings just for testing of the display """
-        self.meetings.append(MockMeeting('Journal Club'))
+        self.meetings.append(MockMeeting(1, 'Journal Club', "Thu 10:00 - 11:00", 5, 5))
+        self.meetings.append(MockMeeting(2, 'Lab Meeting', "Fri 12:00 - 14:00", 42, 5))
 
-class Name(Widget):
-    """Generates a greeting."""
+    def convert_meetings_to_table(self):
+        """converts the meeting array into an array that can be displayed in a table"""
+        self.table_rows = []
+        self.table_rows.append( ("ID", "Name", "Time", "invited", "confirmed" ))
+        for meeting in self.meetings:
+            self.table_rows.append( ( meeting.meeting_id, meeting.name, meeting.time, meeting.num_invited, 
+                             meeting.num_confirmed))
 
-    who = reactive("name", layout=True)  
+    def convert_table_to_meeting(self):
+        """
+        converts the table from TUI into into meeting array that can be pushed to google sheets
+        since this is just mock, it is not implemeneted.
+        """
+        pass
 
-    def render(self) -> str:
-        return self.app.schedule.meetings[0].name
-    
-
-class MeetingDisplay(Static):
-    """A widget to display a scheduled meeting"""
-
-    def compose(self) -> ComposeResult:
-        """Items of a meeting"""
-        yield Input(placeholder="Enter your name")
-        yield Name()
-        yield Button('Update', id='update', variant='primary')
-    
-    def on_input_changed(self, event: Input.Changed) -> None:
-        self.query_one(Name).who = event.value
-        self.app.schedule.meetings[0].name = "Lab Meeting"
-        
 
 class MeetingsApp(App):
     """
     A Textual app to manage meetings.
     """
 
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    BINDINGS = [ ("d", "toggle_dark", "Toggle dark mode"), 
+                ("l" , "load_meetings" , "Load Meetings")]
     CSS_PATH = "./assets/css/meetings.css"
 
     schedule = reactive( MockSchedule("A Mock Schedule"))
@@ -97,15 +97,16 @@ class MeetingsApp(App):
         """Create child widgets for the app."""
         yield Header()
         yield Footer()
-        yield MeetingDisplay()
         yield DataTable()
 
-    def on_mount(self) -> None:
+    def action_load_meetings(self) -> None:
         table = self.query_one(DataTable)
         table.cursor_type = next(cursors)
         table.zebra_stripes = True
+        ROWS = self.app.schedule.table_rows
         table.add_columns(*ROWS[0])
         table.add_rows(ROWS[1:])
+        
 
     def key_c(self):
         table = self.query_one(DataTable)
@@ -114,7 +115,6 @@ class MeetingsApp(App):
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.dark = not self.dark
-
 
 if __name__ == "__main__":
     app = MeetingsApp()
