9331e007f0e80397c2dc074c88525e5beeb7b9ab
Nils <52067471+nils-n@users.noreply.github.com>
2023-05-26 13:34:49 +0100

    replace mock classes in Main App

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
 from textual.app import App, ComposeResult
 from textual.widgets import  Footer, Header, Input, DataTable, Markdown
 from textual.reactive import reactive
-from textual.app import App, ComposeResult
 from itertools import cycle
+from reminding.schedule import Schedule
 
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# terminal : 80 characters wide and 24 rows high
 
 EXAMPLE_MARKDOWN = """\
 # Meeting Reminders
@@ -17,69 +15,17 @@ This Terminal Application helps you organize your upcoming meetings. (Press 'L' 
 
 cursors = cycle(["column", "row", "cell"])
 
-class MockMeeting():
-    """
-    mocks a Meeting class 
-    eventually this class will be imported. For now, this is just a placeholder
-    """
-    def __init__(self, meeting_id, name, time, invited, confirmed ) -> None:
-        self.meeting_id = meeting_id
-        self.name = name
-        self.time = time
-        self.num_invited = invited
-        self.num_confirmed = confirmed
-        self.participants = []
-
-
-class MockSchedule():
-    """
-    mocks a Schedule class 
-    eventually this class will be imported. For now, this is just a placeholder
-    """
-
-    def __init__(self, name, ) -> None:
-        self.name = name
-        self.num_meetings = 0
-        self.meetings = []
-        self.table_rows = []
-        self.load_meetings()
-        self.convert_meetings_to_table()
-
-    def change_name(self, new_name):
-        """ just to test if this method can be called from the main App """
-        self.name = new_name
-
-    def load_meetings(self):
-        """ create mock list of meetings just for testing of the display """
-        self.meetings.append(MockMeeting(1, 'Journal Club', "Thu 10:00 - 11:00", 5, 5))
-        self.meetings.append(MockMeeting(2, 'Lab Meeting', "Fri 12:00 - 14:00", 42, 5))
-
-    def convert_meetings_to_table(self):
-        """converts the meeting array into an array that can be displayed in a table"""
-        self.table_rows = []
-        self.table_rows.append( ("ID", "Name", "Time", "invited", "confirmed" ))
-        for meeting in self.meetings:
-            self.table_rows.append( ( meeting.meeting_id, meeting.name, meeting.time, meeting.num_invited, 
-                             meeting.num_confirmed))
-
-    def convert_table_to_meeting(self):
-        """
-        converts the table from TUI into into meeting array that can be pushed to google sheets
-        since this is just mock, it is not implemeneted.
-        """
-        pass
-
-
 class MeetingsApp(App):
     """
     A Textual app to manage meetings.
+    
     """
 
     BINDINGS = [ ("d", "toggle_dark", "Toggle dark mode"), 
                 ("l" , "load_meetings" , "Load Meetings")]
     CSS_PATH = "./src/reminding/css/meetings.css"
 
-    schedule = reactive( MockSchedule("A Mock Schedule"))
+    schedule = reactive( Schedule("An example Schedule", [], []))
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
