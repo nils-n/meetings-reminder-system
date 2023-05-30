3ed810445ee5955d6c8a2ae1f135134b45f7d424
Nils <52067471+nils-n@users.noreply.github.com>
2023-05-30 14:29:04 +0100

    add a failing test that invalid participant should raise an error

--- tests/test_worksheet.py
+++ tests/test_worksheet.py
@@ -44,3 +44,40 @@ def test_can_read_all_valid_participants_from_worksheet(load_worksheet) -> None:
 
     assert model.valid_participants[0] == valid_participants[0]
     assert model.valid_participants[1] == valid_participants[1]
+
+
+@pytest.mark.parametrize(
+    "fixture_name, participant, expectation",
+    [
+        (
+            "load_worksheet",
+            Participant("Mock Participant 1", "mockemail-1@fakemail.com", 1, True, []),
+            does_not_raise(),
+        ),
+        (
+            "load_worksheet",
+            Participant("Mock Participant 2", "mockemail-2@fakemail.com", 2, True, []),
+            does_not_raise(),
+        ),
+        (
+            "load_worksheet",
+            Participant("Elsa", "elsa@snowmail.com", 42, True, []),
+            pytest.raises(ValueError),
+        ),
+        (
+            "load_worksheet",
+            Participant("Olaf", "olaf@snowmail.com", 100, True, []),
+            pytest.raises(ValueError),
+        ),
+    ],
+)
+def test_invalid_participant_should_raise_ValueError(
+    fixture_name, participant, expectation, request
+) -> None:
+    """Test wheter an participant that is not on the list of allowed /
+    validated participants raises a ValueError"""
+    model = request.getfixturevalue(fixture_name)
+    model.load_valid_participants()
+
+    with expectation:
+        model.is_valid_participant(participant)
