c041df1b881a961cb161a85f9d12d286affc716a
Nils <52067471+nils-n@users.noreply.github.com>
2023-05-30 15:43:18 +0100

    add function to load scheduled meeting into a list of valid Meetings

--- src/reminding/worksheet.py
+++ src/reminding/worksheet.py
@@ -3,6 +3,7 @@ from reminding.participant import Participant
 from reminding.meeting import Meeting
 import gspread
 from google.oauth2.service_account import Credentials
+from datetime import datetime
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -32,6 +33,7 @@ class Worksheet:
         self.schedule_sheet = SHEET.worksheet("schedule")
         self.valid_participants_sheet = SHEET.worksheet("valid-participants")
         self.load_valid_participants()
+        self.unittest_sheet = SHEET.worksheet("unit-test")
 
     def load_valid_participants(self) -> None:
         """loads data of valid Participants from google sheet into a list of Participants
@@ -51,7 +53,12 @@ class Worksheet:
 
     def load_meetings(self, worksheet_name) -> None:
         """Load meetings from the worksheet and transfer into a list of Meetings"""
-        self.meetings = []
+        row_list = self.unittest_sheet.get_all_values()
+        self.meetings = [
+            Meeting(int(row[0]), row[1], datetime.strptime(row[2], "%d/%m/%y %H:%M"))
+            for i, row in enumerate(row_list)
+            if i > 0
+        ]
 
 
 def main() -> None:
