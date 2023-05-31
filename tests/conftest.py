7cfd0f734b09c369c947e1f293a62f78eed599c1
Nils <52067471+nils-n@users.noreply.github.com>
2023-05-31 10:23:29 +0100

    remove cultural references from fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,29 +11,28 @@ import pytest
 @pytest.fixture(scope="session")
 def init_values():
     """fixture to create random init values shared across tests"""
-    random_name = "Han Solo"
-    random_email = "hansolo@fakemail.com"
-    random_id = 42
+    random_name = "Test User 1"
+    random_email = "test.user+1@test.com"
+    random_id = 1
     return random_name, random_email, random_id
 
 
 @pytest.fixture(scope="session")
 def init_participant() -> Participant:
     """initializes a random Participant"""
-    random_name = "Han Solo"
-    random_email = "hansolo@fakemail.com"
-    random_id = 42
+    random_name = "Test User 1"
+    random_email = "test.user+1@test.com"
+    random_id = 1
     return Participant(random_name, random_email, random_id, True)
 
 
 @pytest.fixture(scope="session")
 def create_random_participants() -> List[Participant]:
     """returns a list of participants"""
-    random_names = ["Han Solo", "Darth Vader", "Chewbacca"]
+    number_of_random_participants = 2
+    random_names = [f"Test User {i}" for i in range(number_of_random_participants)]
     random_emails = [
-        "hansolo@fakemail.com",
-        "darthvader@fakemail.com",
-        "chewbacca@fakemail.com",
+        f"test.user+{i}@test.com" for i in range(number_of_random_participants)
     ]
     participants = []
     for i, (name, email) in enumerate(zip(random_names, random_emails)):
@@ -45,23 +44,22 @@ def create_random_participants() -> List[Participant]:
 def create_random_meetings() -> List[Meeting]:
     """initializes and returns a list of random meeting with random values"""
     random_meetings = []
-    num_random_meetings = 2
-    for meeting_id in range(num_random_meetings):
+    number_of_random_meetings = 2
+    number_of_random_participants = 4
+    for meeting_id in range(number_of_random_meetings):
         # create random fake participants
-        random_names = ["Han Solo", "Darth Vader", "Chewbacca"]
+        random_names = [f"Test User {i}" for i in range(number_of_random_participants)]
         random_emails = [
-            "hansolo@fakemail.com",
-            "darthvader@fakemail.com",
-            "chewbacca@fakemail.com",
+            f"test.user+{i}@test.com" for i in range(number_of_random_participants)
         ]
         random_participants = []
         for i, (name, email) in enumerate(zip(random_names, random_emails)):
             random_participants.append(Participant(name, email, i, True))
         # create random fake meeting with these participants
         random_meeting_id = meeting_id
-        random_meeting_name = f"Random Meeting {meeting_id}"
+        random_meeting_name = f"Test Meeting {meeting_id}"
         random_date_time = datetime.now()
-        random_meeting_notes = "This is a random note for this meeting \
+        random_meeting_notes = "This is a test note for this meeting \
             to be sent along with the reminder. "
         random_meetings.append(
             Meeting(
@@ -80,20 +78,18 @@ def create_random_meetings() -> List[Meeting]:
 @pytest.fixture(scope="function")
 def create_random_meeting() -> Meeting:
     """initialize a random meeting with a few random participants"""
-    random_names = ["Hans Gruber", "John McClane", "Tony", "Karl"]
+    random_number_of_participants = 4
+    random_names = [f"Test User {i}" for i in range(random_number_of_participants)]
     random_emails = [
-        "hansgruber@nakatomiplaza.com",
-        "johnmcclane@nakatomiplaza.com",
-        "karl@nakatomiplaza.com",
-        "tony@nakatomiplaza.com",
+        f"test.user+{i}@test.com" for i in range(random_number_of_participants)
     ]
     random_participants = []
     for i, (name, email) in enumerate(zip(random_names, random_emails)):
         random_participants.append(Participant(name, email, i, True))
-    random_name = "Christmas Party"
+    random_name = "Test Meeting"
     random_meeting_id = 5
-    random_datetime = datetime.strptime("24/12/88 21:00", "%d/%m/%y %H:%M")
-    random_notes = "Now I have a machine gun"
+    random_datetime = datetime.strptime("01/01/01 00:00", "%d/%m/%y %H:%M")
+    random_notes = "This is just a Test"
     random_meeting = Meeting(
         random_meeting_id, random_name, random_datetime, True, True, [], random_notes
     )
